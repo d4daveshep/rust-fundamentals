9d3d6dca34fd484d6e1a26fde5ce1d3fb1c0e5d2
d4daveshep <david@daveshep.net.nz>
2026-07-31 13:52:53 +1200

    test 06 done

--- main.rs
+++ main.rs
@@ -5,8 +5,24 @@ fn main() {
     std::io::stdin().read_to_string(&mut input).unwrap();
     let mut lines = input.lines();
 
-    let name = lines.next().unwrap().trim();
-    let age = lines.next().unwrap().trim();
+    // let name = lines.next().unwrap().trim();
+    // let age = lines.next().unwrap().trim();
+    let num = lines.next().unwrap().parse::<i32>().unwrap();
 
-    println!("Hi, {}! You are {} years old.", name, age);
+    // if num % 15 == 0 {
+    //     println!("FizzBuzz")
+    // } else if num % 3 == 0 {
+    //     println!("Fizz")
+    // } else if num % 5 == 0 {
+    //     println!("Buzz")
+    // } else {
+    //     println!("{num}")
+    // }
+
+    match (num % 3 == 0, num % 5 == 0) {
+        (true, true) => println!("FizzBuzz"),
+        (true, false) => println!("Fizz"),
+        (false, true) => println!("Buzz"),
+        (false, false) => println!("{num}"),
+    }
 }
