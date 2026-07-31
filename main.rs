c894bb45f8992932daac873bfd40c0182d925838
d4daveshep <david@daveshep.net.nz>
2026-07-31 21:03:48 +1200

    test 15 done

--- main.rs
+++ main.rs
@@ -1,41 +1,12 @@
-enum Light {
-    Red,
-    Yellow,
-    Green,
-}
-
-impl Light {
-    fn next(&self) -> Self {
-        match self {
-            Light::Red => Light::Green,
-            Light::Yellow => Light::Red,
-            Light::Green => Light::Yellow,
-        }
-    }
-
-    fn name(&self) -> &str {
-        match self {
-            Light::Red => "red",
-            Light::Yellow => "yellow",
-            Light::Green => "green",
-        }
-    }
-}
+use std::num::ParseIntError;
 
 fn main() {
     let mut input = String::new();
     std::io::stdin().read_line(&mut input).unwrap();
 
-    let line = input.trim();
-
-    let current = match line {
-        "red" => Light::Red,
-        "yellow" => Light::Yellow,
-        "green" => Light::Green,
-        _ => Light::Green,
-    };
-
-    let next = current.next();
-
-    println!("{}", next.name());
+    let result = input.trim().parse::<i32>();
+    match result {
+        Ok(num) => println!("ok: {}", num),
+        ParseIntError => println!("error: not a number"),
+    }
 }
