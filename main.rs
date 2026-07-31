348371be75e0e2a6fa7e9f7ed7642898764c14e4
d4daveshep <david@daveshep.net.nz>
2026-07-31 21:17:40 +1200

    fixed test 15

--- main.rs
+++ main.rs
@@ -1,12 +1,19 @@
-use std::num::ParseIntError;
-
 fn main() {
     let mut input = String::new();
     std::io::stdin().read_line(&mut input).unwrap();
 
-    let result = input.trim().parse::<i32>();
+    let result = parse_int(input.trim());
+
     match result {
         Ok(num) => println!("ok: {}", num),
-        ParseIntError => println!("error: not a number"),
+        Err(msg) => println!("error: {}", msg),
     }
+}
+
+fn parse_int(s: &str) -> Result<i32, String> {
+    let result = s
+        .trim()
+        .parse::<i32>()
+        .map_err(|_| String::from("not a number"));
+    result
 }
