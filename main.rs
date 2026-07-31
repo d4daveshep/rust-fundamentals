a86af8034beaf35975d18c28857cd5524e1586e9
d4daveshep <david@daveshep.net.nz>
2026-07-31 12:03:28 +1200

    done

--- main.rs
+++ main.rs
@@ -1,4 +1,12 @@
+use std::io::Read;
+
 fn main() {
-    // Print the greeting below.
-    println!("Hello, Rust!");
+    let mut input = String::new();
+    std::io::stdin().read_to_string(&mut input).unwrap();
+
+    let mut lines = input.lines();
+    let a = lines.next().unwrap().parse::<i32>().unwrap();
+    let b = lines.next().unwrap().parse::<i32>().unwrap();
+
+    println!("{}", a + b);
 }
